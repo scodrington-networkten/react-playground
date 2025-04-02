1c16253fa2ce4b65f36a6f4b111dff953ecf7e30
simoncodrington <scodrington@networkten.com.au>
2025-04-02 17:01:58 +1100

    Added initial react project work

---
+++ project1/first-react/src/main.jsx
@@ -0,0 +1,15 @@
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Navbar from './Navbar.jsx';
+
+const root = document.getElementById('root');
+createRoot(root).render(
+    <StrictMode>
+        <App/>
+        <Navbar/>
+    </StrictMode>,
+)
+
+
---
+++ project1/first-react/src/navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar  = () => {
+    return (
+        <>
+            <nav>
+                <li>
+                    <a>Home</a>
+                </li>
+                <li>
+                    <a>About</a>
+                </li>
+                <li>
+                    <a>Contact</a>
+                </li>
+            </nav>
+        </>
+    )
+}
+export default Navbar
