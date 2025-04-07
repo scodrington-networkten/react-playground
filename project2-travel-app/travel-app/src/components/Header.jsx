8a645edc234bab9b81b9e7c9809a5d4aec2df644
simoncodrington <scodrington@networkten.com.au>
2025-04-07 15:26:27 +1000

    Updates and sass changes for project

---
+++ project2-travel-app/travel-app/src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <>
+            <header className="site-header">
+                <div className="container">
+                    <div className="inner">
+                        Header here
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
+export default Header
