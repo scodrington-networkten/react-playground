40183fa075f3be4aa2ec0af86fdb929521fb56bc
simoncodrington <scodrington@networkten.com.au>
2025-04-08 13:25:24 +1000

    Adjusted props via destructuring

--- project2-travel-app/travel-app/src/components/Entry.jsx
+++ project2-travel-app/travel-app/src/components/Entry.jsx
@@ -1,28 +1,38 @@
 //represents a single entry for the journal
 import "../assets/css/entry.scss";
 
-const Entry = (props) => {
+const Entry = ({data}) => {
 
+    const {
+        location,
+        image,
+        caption,
+        startDate,
+        endDate,
+        description,
+        googleMap
+    } = data;
+    
     return (
         <>
             <article className="entry">
                 <figure className="secondary">
-                    <img src={`../assets/images/${props.image}`} alt="Dynamic"/>
-                    <figcaption>{props.caption}</figcaption>
+                    <img src={`../assets/images/${image}`} alt="Dynamic"/>
+                    <figcaption>{caption}</figcaption>
                 </figure>
                 <section className="primary">
                     <header>
                         <div className="country">
                             <img src="../assets/images/marker.png" alt="location image"/>
                             <p>Japan</p>
-                            <a href={props.googleMap}>View on Google Maps</a>
+                            <a href={googleMap}>View on Google Maps</a>
                         </div>
-                        <h2 className="location">{props.location}</h2>
+                        <h2 className="location">{location}</h2>
                         <p className="dates">
-                            <span className="start">{props.startDate}</span> - <span
-                            className="end">{props.endDate}</span>
+                            <span className="start">{startDate}</span> - <span
+                            className="end">{endDate}</span>
                         </p>
-                        <p className="description">{props.description}</p>
+                        <p className="description">{description}</p>
                     </header>
 
                 </section>
