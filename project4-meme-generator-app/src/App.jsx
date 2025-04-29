117815f337cc52765410ed8d9ef380e7e566ce92
simoncodrington <scodrington@networkten.com.au>
2025-04-29 16:24:55 +1000

    Updated base files

--- project4-meme-generator-app/src/App.jsx
+++ project4-meme-generator-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from '@image-assets/react.svg'
+import viteLogo from '@image-assets/vite.svg'
+import '@css/main.scss'
 
 function App() {
   const [count, setCount] = useState(0)
