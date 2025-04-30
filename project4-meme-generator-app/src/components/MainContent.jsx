eaf9cd97d30c6d41be879ad1df3b60a4519433f3
simoncodrington <scodrington@networkten.com.au>
2025-04-30 15:58:35 +1000

    Improvements with use-effect

--- project4-meme-generator-app/src/components/MainContent.jsx
+++ project4-meme-generator-app/src/components/MainContent.jsx
@@ -1,5 +1,5 @@
 import image from '@image-assets/llama.webp';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 const MainContent = () => {
 
@@ -8,6 +8,14 @@ const MainContent = () => {
         bottomText: "world",
         url: image
     });
+
+    //called to fetch data on load
+    useEffect(function() {
+        getImage();
+    });
+
+
+    const [nameData, setNameData] = useState([]);
 
     //change the top of bottom text
     const handleTextChange = (event) => {
@@ -21,6 +29,30 @@ const MainContent = () => {
         }));
     }
 
+    const getImage = async () => {
+
+        //e.preventDefault();
+        try {
+            const response = await fetch('https://swapi.info/api/people');
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            let items = [];
+            const data = await response.json();
+            data.map(item => {
+               items.push(item.name);
+            })
+
+            setNameData(items);
+
+
+        } catch (error) {
+            throw new Error(`There was an error here: ${error}`);
+        }
+    }
+
 
     return (
         <section className="flex justify-center">
@@ -30,16 +62,19 @@ const MainContent = () => {
                         <label htmlFor="top_text" className="font-medium">Top Text</label>
                         <input type="text" id="top_text" name="top_text" placeholder="witty top text here"
                                className="rounded border border-gray-400 py-1 px-2"
-                               onChange={handleTextChange}/>
+                               onChange={handleTextChange}
+                               value={data.topText}/>
                     </section>
                     <section className="field-section flex flex-col gap-3  w-1/2 flex-[0_0_calc(50%-0.5rem)]">
                         <label htmlFor="bottom_text" className="font-medium">Bottom Text</label>
                         <input type="text" id="bottom_text" name="bottom_text" placeholder="witty bottom text here"
                                className="rounded border border-gray-400 py-1 px-2"
-                               onChange={handleTextChange}/>
+                               onChange={handleTextChange}
+                               value={data.bottomText}/>
                     </section>
                     <section className="button-section w-full">
-                        <button name="generate" id="generate" className="w-full py-3 px-6 rounded">Get a new Image
+                        <button name="generate" onClick={getImage} id="generate"
+                                className="w-full py-3 px-6 rounded">Get a new Image
                         </button>
                     </section>
                     <section className="image-section w-full">
@@ -50,6 +85,11 @@ const MainContent = () => {
                             <span className="bottom" id="bottom_text_image"
                                   className="image-text font-bold uppercase text-5xl text-center absolute inset-x-0 bottom-0 bottom-0 my-5">{data.bottomText}</span>
                         </div>
+                    </section>
+                    <section>
+                        {nameData.length > 0 && <ul>
+                            {nameData.map((item, index) => { return <li key={index}>{item}</li>})}
+                        </ul>}
                     </section>
                 </form>
             </div>
