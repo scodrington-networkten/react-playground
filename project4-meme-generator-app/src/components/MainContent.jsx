96fba5c3ef54bdb585879d25fd8acc67108a304d
simoncodrington <scodrington@networkten.com.au>
2025-04-30 14:52:12 +1000

    Additional work with state

--- project4-meme-generator-app/src/components/MainContent.jsx
+++ project4-meme-generator-app/src/components/MainContent.jsx
@@ -1,4 +1,27 @@
+import image from '@image-assets/llama.webp';
+import {useState} from "react";
+
 const MainContent = () => {
+
+    const [data, setData] = useState({
+        topText: "hello",
+        bottomText: "world",
+        url: image
+    });
+
+    //change the top of bottom text
+    const handleTextChange = (event) => {
+        const {name, value} = event.currentTarget;
+        let propertyName = (name === "top_text") ? "topText" : "bottomText";
+
+        //dynamically change the top or bottom text value based on the field
+        setData(oldData => ({
+            ...oldData,
+            [propertyName]: value
+        }));
+    }
+
+
     return (
         <section className="flex justify-center">
             <div className="container my-4 px-4 py-2">
@@ -6,26 +29,32 @@ const MainContent = () => {
                     <section className="field-section flex flex-wrap flex-col gap-3 w-1/2 flex-[0_0_calc(50%-0.5rem)]">
                         <label htmlFor="top_text" className="font-medium">Top Text</label>
                         <input type="text" id="top_text" name="top_text" placeholder="witty top text here"
-                               className="rounded border border-gray-400 py-1 px-2"/>
+                               className="rounded border border-gray-400 py-1 px-2"
+                               onChange={handleTextChange}/>
                     </section>
                     <section className="field-section flex flex-col gap-3  w-1/2 flex-[0_0_calc(50%-0.5rem)]">
                         <label htmlFor="bottom_text" className="font-medium">Bottom Text</label>
                         <input type="text" id="bottom_text" name="bottom_text" placeholder="witty bottom text here"
-                               className="rounded border border-gray-400 py-1 px-2"/>
+                               className="rounded border border-gray-400 py-1 px-2"
+                               onChange={handleTextChange}/>
                     </section>
                     <section className="button-section w-full">
-                        <button name="generate" id="generate" className="py-3 px-6 rounded">Get a new Image</button>
+                        <button name="generate" id="generate" className="w-full py-3 px-6 rounded">Get a new Image
+                        </button>
                     </section>
                     <section className="image-section w-full">
                         <div className="image-container flex-grow-1 relative">
-                            <img src="http://i.imgflip.com/1bij.jpg" className="w-full" />
-                            <span className="top" className="image-text font-bold uppercase text-5xl text-center absolute inset-x-0 top-0 align-text-center my-5">One does not simply</span>
-                            <span className="bottom" className="image-text font-bold uppercase text-5xl text-center absolute inset-x-0 bottom-0 bottom-0 my-5">Walk into Mordor</span>
+                            <img src={data.url} className="w-full"/>
+                            <span className="top" id="top_text_image"
+                                  className="image-text font-bold uppercase text-5xl text-center absolute inset-x-0 top-0 align-text-center my-5">{data.topText}</span>
+                            <span className="bottom" id="bottom_text_image"
+                                  className="image-text font-bold uppercase text-5xl text-center absolute inset-x-0 bottom-0 bottom-0 my-5">{data.bottomText}</span>
                         </div>
                     </section>
                 </form>
             </div>
         </section>
+
     )
 }
 export default MainContent;
