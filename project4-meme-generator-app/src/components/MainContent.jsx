ff1af5a7584c6cd014eaaea09518136edb9a68c5
simoncodrington <scodrington@networkten.com.au>
2025-04-30 13:32:17 +1000

    updates

--- project4-meme-generator-app/src/components/MainContent.jsx
+++ project4-meme-generator-app/src/components/MainContent.jsx
@@ -2,7 +2,21 @@ const MainContent = () => {
     return (
         <section className="flex justify-center">
             <div className="container my-4 px-4 py-2">
-                <p>Main content here</p>
+                <form className="flex flex-wrap gap-4">
+                    <section className="field-section flex flex-wrap flex-col gap-3 w-1/2 flex-[0_0_calc(50%-0.5rem)]">
+                        <label htmlFor="top_text" className="font-medium">Top Text</label>
+                        <input type="text" id="top_text" name="top_text" placeholder="witty top text here"
+                               className="rounded border border-gray-400 py-1 px-2"/>
+                    </section>
+                    <section className="field-section flex flex-col gap-3  w-1/2 flex-[0_0_calc(50%-0.5rem)]">
+                        <label htmlFor="bottom_text" className="font-medium">Bottom Text</label>
+                        <input type="text" id="bottom_text" name="bottom_text" placeholder="witty bottom text here"
+                               className="rounded border border-gray-400 py-1 px-2"/>
+                    </section>
+                    <section className="w-full">
+                        <button name="generate" id="generate" className="py-3 px-6 rounded">Get a new Image</button>
+                    </section>
+                </form>
             </div>
         </section>
     )
